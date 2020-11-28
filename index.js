3aa0979c2f4473bff90b7ca08daa1c6cd7d75819
ezist <maxwell1998r@gmail.com>
2020-11-27 20:18:43 -0600

    Initial commit
    Signed-off-by: ezist <maxwell1998r@gmail.com>

---
+++ index.js
@@ -0,0 +1,28 @@
+const { Plugin } = require('powercord/entities')
+
+module.exports = class QuickToggleThemes extends Plugin {
+  async startPlugin () {
+    this._themesEnabled = true
+    document.addEventListener('keydown', this.onKeyDownWrapper.bind(this))
+  }
+
+  pluginWillUnload () {
+    document.removeEventListener(this.onKeyDownWrapper.bind(this))
+  }
+
+  onKeyDown (e) {
+    if (e.code !== 'F7') return
+    powercord.styleManager.themes.forEach(v => v.entityID && powercord.styleManager[this._themesEnabled ? 'disable' : 'enable'](v.entityID))
+
+    let moduleManager = powercord.pluginManager.get('pc-moduleManager')
+    if (this._themesEnabled)
+      document.querySelector('#powercord-quickcss').remove()
+    else moduleManager._loadQuickCSS.bind(moduleManager)()
+
+    this._themesEnabled = !this._themesEnabled
+  }
+
+  onKeyDownWrapper (e) {
+    this.onKeyDown(e)
+  }
+}
